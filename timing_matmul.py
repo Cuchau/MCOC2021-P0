000a0ab2438ff10dda4d7eb867803df2c58fe56a
Bruno Riquelme <88348645+Cuchau@users.noreply.github.com>
2021-08-05 16:20:56 -0400

    Add files via upload

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,13 +1,29 @@
 from numpy import zeros
 from time import perf_counter
+import matplotlib as plt
 
-N = 10000
-A = zeros((N,N))+1
-B = zeros((N,N))+2
+N = 3
+Ns = [10,100,1000,2000,3000,4000,5000,6000,7000,8000]
+dts = []
+memt = []
+archivo= open("rendimiento.txt","w")
 
-t1 = perf_counter()
-C =A@B
-t2 = perf_counter()
-dt= t2-t1
+for i in range (10):
+    for N in Ns:
+        uso_memoria_total = 0
+        A = zeros((N,N))+1
+        B = zeros((N,N))+2
+        
+        t1 = perf_counter()
+        C =A@B
+        t2 = perf_counter()
 
-print(f"dt={dt} s")
+        uso_memoria_total= A.nbytes + B.nbytes + C.nbytes
+        dt= t2-t1
+        dts.append(dt)
+        memt.append(uso_memoria_total)
+        print(f"N = {N} dt = {dt} s mem = {uso_memoria_total} bytes")
+        archivo.write(f"{N} {dt} {uso_memoria_total}\n")
+    
+archivo.close()
+
